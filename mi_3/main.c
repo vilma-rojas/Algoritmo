020adc47866587a0f0025cc0ae88e49b0b53d342
vilma-rojas <vilma_diana@outlook.com>
2021-08-21 17:36:28 -0300

    tercer ejercicio

--- mi_3/main.c
+++ mi_3/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+A partir de un valor entero ingresado por teclado, se pide informar:
+a) La quinta parte de dicho valor
+b) El resto de la división por 5
+c) La séptima parte del resultado del punto a)
+*/
+
 int main()
 {
-    printf("Hello world!\n");
+    int partes(int, int);
+    int restoDeLaDivisionPor5(int);
+
+    int num;
+
+    printf ("Introuzca numero: ");
+    scanf ("%d",&num);;
+
+    printf("a) La quinta parte es: %d\n", partes(num, 5));
+    printf("a) El resto de la division es: %d\n", restoDeLaDivisionPor5(num));
+    printf("a) La septima parte es: %d\n", partes(partes(num, 5), 6));
     return 0;
+}
+
+//b)
+int restoDeLaDivisionPor5(int num){
+    int resto = num % 5 ;
+    return resto ;
+}
+//a) c)
+int partes(int num, int parte){
+    return num / parte;
 }
